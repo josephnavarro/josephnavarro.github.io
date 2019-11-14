33b2261238adee285af46949c3e87c34d73e8d13
navarro0 <navarro0@purdue.edu>
2019-11-14 11:40:17 -0600

    Update utility.js

--- fe3h/assets/scripts/utility.js
+++ fe3h/assets/scripts/utility.js
@@ -36,10 +36,13 @@ function addPhysics(sprite, w=0, h=0, dx=0, dy=0, immovable=false)
 		h = sprite.height;
 	}
 
-	//w *= RATIO;
-	//h *= RATIO;
+	w *= RATIO;
+	h *= RATIO;
 	//dx *= RATIO;
 	//dy *= RATIO;
+	h = 1;
+	dx = 0;
+	dy = 0;
 
 	sprite.body.setSize(w, h, dx, dy);
 
